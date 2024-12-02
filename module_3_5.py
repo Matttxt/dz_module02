1a8fddaa2d56bce027da1673913a4f19c5f70c5e
Matttxt <123890216+Matttxt@users.noreply.github.com>
2024-12-02 19:05:48 +0400

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,14 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if (len(str_number)) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    elif first % 10 == 0:
+        return 1
+    else:
+        return first
+
+result1 = (get_multiplied_digits(40203))
+print(result1)
+result2 = (get_multiplied_digits(402030))
+print(result2)
